22f56288b1a9c398e6fa8e7c1b94dee7013ac348
isakshamkumar2 <saksham@genflowly.com>
2024-08-25 19:44:21 +0530

    [FEATURE] Added cdk code to deploy the authentication service

---
+++ cdk/lib/auth-service-stack.ts
@@ -0,0 +1,36 @@
+import { Construct } from 'constructs';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
+import { AuthenticationServiceVpc } from './constructs/vpc';
+import { AuthenticationServiceSecurityGroup } from './constructs/security-group';
+import { AuthenticationServiceDynamoDb } from './constructs/dynamodb';
+import { AuthenticationServiceEc2Instance } from './constructs/ec2-instance';
+import { AuthenticationServiceIamPolicies } from './constructs/iam-policies';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { AUTHENTICATION_SERVICE_NAME, DELMITER, EC2_INSTANCE_PUBLIC_IP_OUTPUT, PACKAGE_DEPLOYMENT_BUCKET_ID, PACKAGE_DEPLOYMENT_BUCKET_BETA_NAME } from './constants';
+
+export class AuthenticationService extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const vpc = new AuthenticationServiceVpc(this, `${AUTHENTICATION_SERVICE_NAME}${DELMITER}VPC`);
+    const securityGroup = new AuthenticationServiceSecurityGroup(this, `${AUTHENTICATION_SERVICE_NAME}${DELMITER}SecurityGroup`, { vpc: vpc.vpc });
+    const dynamoDb = new AuthenticationServiceDynamoDb(this, `${AUTHENTICATION_SERVICE_NAME}${DELMITER}DDB`);
+    const ec2Instance = new AuthenticationServiceEc2Instance(this, `${AUTHENTICATION_SERVICE_NAME}${DELMITER}EC2Instance`, {
+      vpc: vpc.vpc,
+      securityGroup: securityGroup.securityGroup,
+      dynamoDbTable: dynamoDb.table,
+    });
+
+    const wheelBucket = Bucket.fromBucketName(this, PACKAGE_DEPLOYMENT_BUCKET_ID, PACKAGE_DEPLOYMENT_BUCKET_BETA_NAME);
+    new AuthenticationServiceIamPolicies(this, `${AUTHENTICATION_SERVICE_NAME}${DELMITER}IAMPolicies`, {
+      ec2Instance: ec2Instance.instance,
+      dynamoDbTable: dynamoDb.table,
+      s3Bucket: wheelBucket,
+    });
+
+    new CfnOutput(this, EC2_INSTANCE_PUBLIC_IP_OUTPUT, {
+      value: ec2Instance.instance.instancePublicIp,
+      description: `Public IP address of the ${AUTHENTICATION_SERVICE_NAME} EC2 instance`,
+    });
+  }
+}
---
+++ cdk/lib/constructs/iam-policies.ts
@@ -0,0 +1,62 @@
+import { Construct } from 'constructs';
+import { Instance } from 'aws-cdk-lib/aws-ec2';
+import { IBucket } from 'aws-cdk-lib/aws-s3';
+import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+import {
+  createPolicyStatement,
+  attachManagedPolicyToRole,
+  attachCustomPolicyStatementsToPrincipalPolicy,
+  MANAGED_POLICIES
+} from '@genflowly/cdk-commons';
+
+export interface AuthenticationServiceIamPoliciesProps {
+  ec2Instance: Instance;
+  dynamoDbTable: ITable;
+  s3Bucket: IBucket;
+}
+
+export class AuthenticationServiceIamPolicies extends Construct {
+  constructor(scope: Construct, id: string, props: AuthenticationServiceIamPoliciesProps) {
+    super(scope, id);
+
+    const s3ListBucketPolicy = createPolicyStatement(
+      ['s3:ListBucket'],
+      [props.s3Bucket.bucketArn]
+    );
+
+    const s3GetObjectPolicy = createPolicyStatement(
+      ['s3:GetObject'],
+      [`${props.s3Bucket.bucketArn}/*`]
+    );
+
+    const secretsManagerPolicy = createPolicyStatement(
+      [
+        'secretsmanager:GetSecretValue',
+        'secretsmanager:DescribeSecret'
+      ],
+      ['*']
+    );
+
+    const dynamoDbPolicy = createPolicyStatement(
+      [
+        'dynamodb:GetItem',
+        'dynamodb:PutItem',
+        'dynamodb:UpdateItem',
+        'dynamodb:DeleteItem',
+        'dynamodb:Query',
+        'dynamodb:Scan'
+      ],
+      [props.dynamoDbTable.tableArn]
+    );
+
+    attachManagedPolicyToRole(props.ec2Instance.role, MANAGED_POLICIES.AmazonDynamoDBFullAccess);
+    attachManagedPolicyToRole(props.ec2Instance.role, MANAGED_POLICIES.SecretsManagerReadWrite);
+
+    attachCustomPolicyStatementsToPrincipalPolicy(props.ec2Instance.role, [
+      s3ListBucketPolicy,
+      s3GetObjectPolicy,
+      secretsManagerPolicy,
+      dynamoDbPolicy
+    ]);
+  }
+} 
